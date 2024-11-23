7f84b467af540b9dfe379492538746f232273285
halit uzan <halit.uzan@gmail.com>
2024-11-24 01:43:23 +0300

    sorting expense table data

--- src/components/Expense/index.tsx
+++ src/components/Expense/index.tsx
@@ -7,6 +7,7 @@ import {
   setExpenses,
   setExpenseValues
 } from "@/lib/features/expenditure";
+import { Icon } from "@iconify/react";
 import getExpensesCategories from "@/services/Categories/getExpensesCategories";
 import getExpense from "@/services/Expense/getExpense";
 import setExpense from "@/services/Expense/setExpense";
@@ -16,6 +17,9 @@ import React, { FC, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 import HistoriesAddAction from "../HistoriesAddAction";
+import sortByDate from "@/helpers/sortByDate";
+import filterByDateRange from "@/helpers/filteredByDateRange";
+import DateRangePicker from "../UI/DateRangePicker";
 
 const Expense: FC = () => {
   const t = useTranslations("Expense")
@@ -25,10 +29,18 @@ const Expense: FC = () => {
   const expenseValues = useSelector(selectExpenseValues);
   const { amount, categoryId, date, description } = expenseValues;
   const [expenseCategories, setExpenseCategories] = useState<Category[]>([]);
+  const [dateRange, setDateRange] = useState<Date[] | undefined[]>([undefined, undefined]);
+  const [sort, setSort] = useState<"asc" | "desc" | null>(null);
 
   const dispatchHandler = (key: keyof ExpenditureValues, value: string) => {
     dispatch(setExpenseValues({ key: key, value: value }));
   };
+
+  const handleSortClick = () => {
+    const nextOrder = sort === null ? "desc" : sort === "desc" ? "asc" : null;
+    setSort(nextOrder);
+  };
+
 
   const addExpense = (e: React.FormEvent) => {
     e.preventDefault();
@@ -48,24 +60,57 @@ const Expense: FC = () => {
       dispatch(clearExpenseValues());
     }
   };
-
-  useEffect(() => {
-    const data = getExpensesCategories();
-    setExpenseCategories(data);
-  }, []);
-
-  useEffect(() => {
+  const setExpenseDatas = () => {
     const expensesData = getExpense();
     dispatch(setExpenses(expensesData));
-  }, []);
+  }
+  const handleFilterTime = (sorting: any) => {
+
+    if (sorting === null) {
+      setExpenseDatas()
+    } else {
+      let copyData = [...expenses]
+      const data = sortByDate(copyData, sorting)
+      dispatch(setExpenses(data));
+    }
+
+  }
+  useEffect(() => {
+    handleFilterTime(sort)
+  }, [sort]);
+  useEffect(() => {
+    if (dateRange[0] || dateRange[1]) {
+      const data = filterByDateRange(expenses, dateRange[0], dateRange[1])
+      dispatch(setExpenses(data));
+    } else {
+      const data = getExpensesCategories();
+      setExpenseCategories(data);
+      setExpenseDatas()
+    }
+  }, [dateRange]);
+
 
   return (
     <div className='container mx-auto p-4'>
       <HistoriesAddAction buttonColor={"expenses"} buttonText={tf("Expense.button")} values={expenseValues} categoryData={expenseCategories} dispatchHandler={dispatchHandler} addAction={addExpense} title={t("title")} />
 
-
       <div className='bg-slate-100 dark:bg-primary/80 p-6 rounded-lg shadow-md'>
-        <h2 className='text-2xl font-semibold mb-4 text-primary dark:text-slate-50'>{t("history")}</h2>
+        <div className='flex flex-col md:flex-row justify-between items-center w-full mb-2'>
+          <h2 className='text-2xl font-semibold mb-4 text-primary dark:text-slate-50'>{t("history")}</h2>
+          <div className="flex items-center">
+            <button onClick={() => {
+              setExpenseDatas()
+              setDateRange([undefined, undefined])
+            }} className="mr-2 text-primary dark:text-slate-100">Clear</button>
+            <DateRangePicker dateRange={dateRange} setDateRange={setDateRange} />
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between items-center w-full p-1 bg-primary text-slate-100 dark:bg-slate-500 dark:text-slate-100">
+          <div>Tür</div>
+          <button onClick={handleSortClick} className="flex items-center">
+            Tarihe Göre Sırala <Icon icon="bx:sort" />
+          </button>
+        </div>
         {expenses.length === 0 ? (
           <p className='text-primary dark:text-slate-50'>{t("noData")}</p>
         ) : (
