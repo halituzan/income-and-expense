ac9f09b85e252dd9f10b15599ada11bb26476137
halit uzan <halit.uzan@gmail.com>
2024-11-24 15:09:26 +0300

    added incomes export function

--- src/components/Incomes/index.tsx
+++ src/components/Incomes/index.tsx
@@ -7,6 +7,7 @@ import {
   setIncomes,
   setIncomeValues,
 } from "@/lib/features/expenditure";
+import * as XLSX from "xlsx";
 import { Icon } from "@iconify/react";
 import getIncomesCategories from "@/services/Categories/getIncomesCategories";
 import getIncome from "@/services/Income/getIncome";
@@ -24,6 +25,7 @@ import sortByDate from "@/helpers/sortByDate";
 const Incomes = () => {
   const t = useTranslations("Income")
   const tf = useTranslations("Form")
+  const te = useTranslations("Exports");
   const dispatch = useDispatch();
   const incomes = useSelector(selectIncomes);
   const expenseValues = useSelector(selectIncomeValues);
@@ -88,6 +90,18 @@ const Incomes = () => {
       setIncomeDatas()
     }
   }, [dateRange]);
+  const handleExport = () => {
+    const formattedData = incomes.map((income: any) => ({
+      [te("category")]: income.category.name,
+      [te("description")]: income.description,
+      [te("amount")]: income.amount.toFixed(2) + " TL",
+      [te("date")]: new Date(income.date).toLocaleString(),
+    }));
+    const worksheet = XLSX.utils.json_to_sheet(formattedData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Incomes");
+    XLSX.writeFile(workbook, "incomes.xlsx");
+  };
 
   return (
     <div className='container mx-auto p-4'>
@@ -95,13 +109,32 @@ const Incomes = () => {
 
       <div className='bg-slate-100 dark:bg-primary/80 p-6 rounded-lg shadow-md'>
         <div className='flex flex-col md:flex-row justify-between items-center w-full mb-2'>
-          <h2 className='text-2xl font-semibold mb-4 text-primary dark:text-slate-50'>{t("history")}</h2>
-          <div className="flex items-center">
-            <button onClick={() => {
-              setIncomeDatas()
-              setDateRange([undefined, undefined])
-            }} className="mr-2 text-primary dark:text-slate-100">Clear</button>
-            <DateRangePicker dateRange={dateRange} setDateRange={setDateRange} />
+          <div className='flex items-center mb-4'>
+            <h2 className='text-2xl font-semibold  text-primary dark:text-slate-50'>
+              {t("history")}
+            </h2>
+            <button
+              className='ml-2 bg-primary text-slate-100 dark:bg-slate-200 dark:text-primary p-2 rounded-lg'
+              onClick={handleExport}
+            >
+              {te("download")}
+            </button>
+          </div>
+
+          <div className='flex items-center'>
+            <button
+              onClick={() => {
+                setIncomeDatas();
+                setDateRange([undefined, undefined]);
+              }}
+              className='mr-2 text-primary dark:text-slate-100'
+            >
+              Clear
+            </button>
+            <DateRangePicker
+              dateRange={dateRange}
+              setDateRange={setDateRange}
+            />
           </div>
         </div>
         <div className="flex flex-1 justify-between items-center w-full p-1 bg-primary text-slate-100 dark:bg-slate-500 dark:text-slate-100">
